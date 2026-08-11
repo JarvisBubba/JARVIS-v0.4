5e4d0bf36b5fb2fadc7a8d52d9fa5dd41b44b614
JarvisBubba <jkiklolxdtytysm@gmail.com>
2026-08-11 16:19:00 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C="jarvis-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./index.html","./chat.html","./manifest.webmanifest","./icon.svg"])));self.skipWaiting();});
+self.addEventListener("activate",e=>self.clients.claim());
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
+ e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request)));});
